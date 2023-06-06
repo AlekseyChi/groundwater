0963b693f5ee87e5ecd02241cdbc75a9a28f1602
cawsW <friclox@gmail.com>
2023-06-06 03:08:25 +0300

    nginx client size

--- config/settings/production.py
+++ config/settings/production.py
@@ -61,6 +61,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", def
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
+FILE_UPLOAD_PERMISSIONS = 0o644
 
 # EMAIL
 # ------------------------------------------------------------------------------
