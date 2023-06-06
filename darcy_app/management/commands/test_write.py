792d5a0fd918141f3e9d51eb4738e539eaafccae
cawsW <friclox@gmail.com>
2023-06-06 03:00:41 +0300

    nginx client size

---
+++ darcy_app/management/commands/test_write.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.files.storage import default_storage
+
+
+class Command(BaseCommand):
+    help = 'Tests write permissions for media directory'
+
+    def handle(self, *args, **options):
+        file_name = 'test.txt'
+        file_content = 'This is a test file.'
+
+        if default_storage.exists(file_name):
+            self.stdout.write(self.style.WARNING(f'File {file_name} already exists'))
+        else:
+            default_storage.save(file_name, ContentFile(file_content))
+            self.stdout.write(self.style.SUCCESS(f'Successfully written to {file_name}'))
+
